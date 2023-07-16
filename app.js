d47edfa0fd2b5347d762b936a2756c3aa35baace
Brayan <brayanmancot@gmail.com>
2023-07-16 07:58:57 -0500

    imprimir las opciones con un ciclo do while

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+require("colors");
 
-require('colors');
-
-const mostrarMenu = require('./helpers/mensajes');
+const { mostrarMenu, pause } = require("./helpers/mensajes");
 
 console.clear();
 
-const main = async () =>{
-    console.log('Hola mundo');
+const main = async () => {
+  console.log("Hola mundo");
 
-    mostrarMenu();
-}
+  let opt = '';
 
+  do {
+    opt = await mostrarMenu();
+    console.log({ opt });
 
+    if( opt !== '0') { await pause();}
+
+  } while (opt !== "0");
+};
 
 main();
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,31 +1,52 @@
-require('colors');
+const { rejects } = require("assert");
+const { resolve } = require("path");
 
-const mostrarMenu = ()=>{
+require("colors");
 
+const mostrarMenu = () => {
+  //esto es una primesa que devolvera un objecto
+  return new Promise((resolve) => {
     console.clear();
-    console.log('============================='.green);
-    console.log('    Seleccione una opción    '.green);
-    console.log('=============================\n'.green);
+    console.log("=============================".green);
+    console.log("    Seleccione una opción    ".green);
+    console.log("=============================\n".green);
 
-    console.log(`${ '1.'.green } Crear tarea`);
-    console.log(`${ '2.'.green } Listar tareas`);
-    console.log(`${ '3.'.green } Listar tareas completas`);
-    console.log(`${ '4.'.green } Listar tareas pendientes`);
-    console.log(`${ '5.'.green } Completar tarea(s)`);
-    console.log(`${ '6.'.green } Borrar tarea`);
-    console.log(`${ '0.'.green } Salir\n`);
-
+    console.log(`${"1.".green} Crear tarea`);
+    console.log(`${"2.".green} Listar tareas`);
+    console.log(`${"3.".green} Listar tareas completas`);
+    console.log(`${"4.".green} Listar tareas pendientes`);
+    console.log(`${"5.".green} Completar tarea(s)`);
+    console.log(`${"6.".green} Borrar tarea`);
+    console.log(`${"0.".green} Salir\n`);
 
     //se crea un interface para revisvir infomacion del usuario
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout,
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
     });
 
-    readline.question('Seleccione una opción: ', (opt) => {
-        console.log({ opt });
-        readline.close();
-    })
-}
+    readline.question("Seleccione una opción: ", (opt) => {
+      readline.close();
+      resolve(opt);
+    });
+  });
+};
 
-module.exports = mostrarMenu;
+const pause = () => {
+  return new Promise((resolve) => {
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    readline.question(`\nPresione ${"ENTER".green} para continuar\n`, (opt) => {
+      readline.close();
+      resolve();
+    });
+  });
+};
+
+module.exports = {
+  mostrarMenu,
+  pause,
+};
